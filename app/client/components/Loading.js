072b6d62d9547796dfd5e4265673b72f521ee5f6
alexander.guzman@serpicodev.com <alexander.guzman@serpicodev.com>
2019-02-18 00:56:42 -0700

    more loadings

--- app/client/components/Loading.js
+++ app/client/components/Loading.js
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = ({ show, text }) => {
     return show &&
         <div>
-            <object data="/assets/img/loading.svg" />
+            <object data="/assets/img/loading.spinner.svg" />
             <span style={{ fontSize: 20 }}>{text || "LOADING.."}</span>
         </div>
 }
